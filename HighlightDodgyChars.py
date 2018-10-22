2ce02c69fc1d1fa3bd1417948a911f1ad7fc03be
lukaw3d <lukaw3d@gmail.com>
2018-10-22 14:20:15 +0200

    Speedup highlighting large files by checking only around cursor

--- HighlightDodgyChars.py
+++ HighlightDodgyChars.py
@@ -39,6 +39,20 @@ class HighlightDodgyChars(sublime_plugin.EventListener):
             self.delay_update = True
             sublime.set_timeout(lambda: self.end_cooldown(view), 250)
 
+    def on_load_async(self, view):
+        self.highlight(view)
+
+    def on_selection_modified_async(self, view):
+        # call highlight max 4 times a second
+        if self.delay_update:
+            # if a modification happens during cooldown, an update is needed afterwards
+            self.has_been_modified = True
+        else:
+            self.highlight(view)
+            # 250 ms cooldown
+            self.delay_update = True
+            sublime.set_timeout(lambda: self.end_cooldown(view), 250)
+
     def end_cooldown(self, view):
         self.delay_update = False;
         if self.has_been_modified:
@@ -54,7 +68,7 @@ class HighlightDodgyChars(sublime_plugin.EventListener):
         needle = '[^\x00-\x7F' + default_whitelist + self.whitelist + ']'
 
         # search the view
-        for pos in self.view.find_all(needle):
+        for pos in self.find_regions(view, needle):
             highlights.append(pos)
             phantoms.append(sublime.Phantom(pos, '<span style="color: var(--background); background-color: var(--pinkish); border-radius: 3px;">!</span>', sublime.LAYOUT_INLINE))
 
@@ -65,3 +79,28 @@ class HighlightDodgyChars(sublime_plugin.EventListener):
 
         # if something dodgy was found, highlight the dodgy parts
         self.phantom_set.update(phantoms);
+
+
+    def find_regions(self, view, search):
+        limit_chars = 50000
+
+        regions = []
+        if not limit_chars:
+            regions += view.find_all(search, Pref.case_sensitive)
+        else:
+            chars = limit_chars
+            visible_region = view.visible_region()
+            begin = 0 if visible_region.begin() - chars < 0 else visible_region.begin() - chars
+            end = visible_region.end() + chars
+            from_point = begin
+            while True:
+                region = view.find(search, from_point)
+                if region:
+                    regions.append(region)
+                    if region.end() > end:
+                        break
+                    else:
+                        from_point = region.end()
+                else:
+                    break
+        return regions
